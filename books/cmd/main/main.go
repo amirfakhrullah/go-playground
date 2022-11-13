b12e2cf02a89a7abbeb883bacaf5eeb3d8b606d3
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2022-11-13 22:20:55 +0800

    books-db done

--- books/cmd/main/main.go
+++ books/cmd/main/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"github.com/amirfakhrullah/go-playground/tree/main/books/pkg/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	r := mux.NewRouter()
 	routes.BookRoutes(r)
 	http.Handle("/", r)
